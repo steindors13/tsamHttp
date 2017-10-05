49985ed1d75dff3757a9dd019cdac487e9ed1649
steindors13 <steindors13@skel.ru.is>
2017-10-05 14:33:41 +0000

    log fil ettuskit

--- tsam17 2/pa2/src/httpd.c
+++ tsam17 2/pa2/src/httpd.c
@@ -10,6 +10,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <glib.h>
+#include <time.h>
 
 char webpage[] =
 "HTTP/1.1 200 OK\r\n"
@@ -19,12 +20,41 @@ char webpage[] =
 "<body><h1>An IP address should be here, plus the port number</h1><br>\r\n"
 "</body></html>\r\n";
 
-void printClientAddr(&struct sockaddr_in address)
+void write_logfile(int client, char* ip_addr, int port_nr)
+{
+	FILE *fp;
+	FILE *f;
+        printf("Got client connection.......\n");
+        char buff_cli[8192];
+	f = fopen("x.log", "a+");
+	fprintf(f, "client ip: %s ", ip_addr);
+	fprintf(f, "client port: %d ", port_nr);
+
+        fp = fdopen(client, "r");
+        char* data = fgets(buff_cli, sizeof(buff_cli), fp);
+	data = strtok(data, " \r\n");
+	fprintf(f, "Request is: %s ", data);
+
+	time_t timer;
+    	char time_buff[26];
+    	struct tm* time_info;
+
+    	time(&timer);
+    	time_info = localtime(&timer);
+
+    	strftime(time_buff, 26, "%Y-%m-%d %H:%M:%S", time_info);
+    	puts(time_buff);
+	fprintf(f, "Timestamp: %s ", time_buff);
+	fclose(f);
+	
+}
+
+/*void printClientAddr(&struct sockaddr_in address)
 {
 	
 	unsigned char *ip = (unsigned char *)&client_addr;
 	printf("hahah %d.%d.%d.%d\n", ip[0], ip[1], ip[2], ip[3]);
-}
+}*/
  
 
 int main(int argc, char *argv[])
@@ -32,9 +62,10 @@ int main(int argc, char *argv[])
 	struct sockaddr_in server_addr, client_addr;  // internet address
 	socklen_t sin_len = sizeof(client_addr);  // size of address
 	int fd_server, fd_client; 
-	//char buff_cli[8192];
-	//char buff_serv[8192];
+	//char buff1[10000];
+	//char buff2[200];
 	int on = 1;
+	char* ip_addr;
 	int port = strtol(argv[1], NULL, 10);
 	printf("Starting server, %d arguments\n", argc);
 	
@@ -73,31 +104,15 @@ int main(int argc, char *argv[])
 	while(1)
 	{
 		fd_client = accept(fd_server, (struct sockaddr *) &client_addr, &sin_len);
-
+		ip_addr = inet_ntoa(client_addr.sin_addr);
 		if(fd_client < 0)
 		{
 			perror("Connection failed.......");
 			continue;
 		}
-
-		printf("Got client connection.......\n");
-		int c;
-		FILE *fp;
-		FILE *log;
-		log = fopen("log.log", "r");
-		fp = fdopen(fd_client, "r");
-		if(!fp) 
-		{
-			printf("Error reading file, while processing http");
-			exit(1);
-		} 
-		else
-		{
-			while( (c = getc(fp)) != EOF)
-				putchar(c);
-			
-			fclose(fp);
-		}
+		
+		printf("Got client connection.......\n");	
+		write_logfile(fd_client, ip_addr, port);
 		
 		printf("closing...\n");
 		
